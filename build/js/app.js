3f8400f9c6902e236bb281fa16e631b916170da1
Viktoriia <babenko_v_v@ukr.net>
2016-07-01 19:20:57 +0300

    upd

--- build/js/app.js
+++ build/js/app.js
@@ -37,6 +37,24 @@ $(document).ready(function() {
   cssEase: 'linear'
 	});
 
+	$('.js-toggle-link').on('click', function(){
+			var	parent = $('.sidebar__menu');
+			var link = $(this).parent('.sidebar__cell');
+			var child = parent.find('.is-active');
+			var	navPrev = parent.find('.sidebar__submenu');
+			var	navActive = $(this).next('.sidebar__submenu');
+			if (child.hasClass('is-active')) {
+				navPrev.slideUp(700);
+				child.removeClass('is-active');
+				link.addClass('is-active');
+				navActive.slideDown(700);
+				return false;
+			} else {
+				link.addClass('is-active');
+				navActive.slideDown(700);
+			}
+		return false
+		});
 
 
 
