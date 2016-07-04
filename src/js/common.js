cb779aad32059cd5a2bcb9c0b003728b224e1dfe
Viktoriia <Babenko_v_v@ukr.net>
2016-07-04 17:37:27 +0300

    upd

--- src/js/common.js
+++ src/js/common.js
@@ -19,18 +19,20 @@ $(document).ready(function() {
 			var child = parent.find('.is-active');
 			var	navPrev = parent.find('.sidebar__submenu');
 			var	navActive = $(this).next('ul');
-			if (child.hasClass('is-active')) {
-				navPrev.slideUp(700);
-				child.removeClass('is-active');
-				link.addClass('is-active');
-				navActive.slideDown(700);
-			} else {
-				link.addClass('is-active');
-				navActive.slideDown(700);
-			}
-		return false
-		});
-
+					if (navActive.css('display') == 'none')
+			        {
+									child.removeClass('is-active');
+									navPrev.slideUp(700);
+			            navActive.animate({height: 'show'}, 800);
+									link.addClass('is-active');
+			        }
+			    else
+			        {
+			            navActive.animate({height: 'hide'}, 800);
+									link.removeClass('is-active');
+			        }
+					return false;
+	});
 
 	$('.btn-menu').on('click', function(){
 		var menu = $('.header__menu');
