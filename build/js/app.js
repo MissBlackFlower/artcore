1100d14ead67ee6ad48aa85ff501b43d2b2c1e3e
Viktoriia <Babenko_v_v@ukr.net>
2016-07-04 08:17:38 +0300

    upd

--- build/js/app.js
+++ build/js/app.js
@@ -39,16 +39,15 @@ $(document).ready(function() {
 
 	$('.js-toggle-link').on('click', function(){
 			var	parent = $('.sidebar__menu');
-			var link = $(this).parent('.sidebar__cell');
+			var link = $(this).parent('li');
 			var child = parent.find('.is-active');
 			var	navPrev = parent.find('.sidebar__submenu');
-			var	navActive = $(this).next('.sidebar__submenu');
+			var	navActive = $(this).next('ul');
 			if (child.hasClass('is-active')) {
 				navPrev.slideUp(700);
 				child.removeClass('is-active');
 				link.addClass('is-active');
 				navActive.slideDown(700);
-				return false;
 			} else {
 				link.addClass('is-active');
 				navActive.slideDown(700);
@@ -57,6 +56,37 @@ $(document).ready(function() {
 		});
 
 
-
+	$('.btn-menu').on('click', function(){
+		var menu = $('.header__menu');
+		menu.toggleClass('open-menu')
+	});
+	$('.btn-nav').on('click', function(){
+		var menu = $('.header__nav');
+		menu.toggleClass('open-nav')
+	});
+	$('.types__btn').on('click', function(){
+		var list = $('.types__list');
+		if (list.css('display') == 'none')
+        {
+            list.animate({height: 'show'}, 800);
+        }
+    else
+        {
+            list.animate({height: 'hide'}, 800);
+        }
+			return false;
+	});
+	$('.content__btn').on('click', function(){
+		var list = $('.sidebar__menu');
+		if (list.css('display') == 'none')
+        {
+            list.animate({height: 'show'}, 800);
+        }
+    else
+        {
+            list.animate({height: 'hide'}, 800);
+        }
+			return false;
+	});
 
 });
